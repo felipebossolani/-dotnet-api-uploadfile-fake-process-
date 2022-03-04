44629b1af0f866f2fdc6c3694c29f04e4892ec1c
Felipe <felipe@meuportfol.io>
2022-03-03 22:59:45 -0300

    WebApi enviando o post de arquivo sem logica no meio

--- FileUploadApi/Program.cs
+++ FileUploadApi/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.AspNetCore.Mvc;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
@@ -10,5 +12,34 @@ app.UseSwaggerUI();
 app.UseHttpsRedirection();
 
 app.MapGet("/", () => "Hello World!");
+
+app.MapPost("/files", async Task<IResult> (HttpRequest request, string? tamanho, bool? paisagem) =>
+{
+    if (!request.HasFormContentType)
+        return Results.BadRequest();
+
+    var form = await request.ReadFormAsync();
+    var file = form.Files.First();
+
+    var ticketId = Guid.NewGuid();
+    var fileName = $@"{ticketId.ToString().Substring(10)}-{file.FileName}";
+
+    return Results.Ok(ticketId);
+});
+
+app.MapPost("/", async
+    ([FromForm(Name = "file")] IFormFile file) =>
+{
+    if (file == null) throw new ArgumentNullException(nameof(IFormFile), "Informe o arquivo");
+
+    var tickerId = Guid.NewGuid();
+    var fileName = $@"{tickerId.ToString().Substring(10)}-{file.FileName}";
+
+    return Results.Created("", tickerId);
+})
+.Accepts<IFormFile>("multipart/form-data")
+.Produces(StatusCodes.Status201Created)
+.Produces(StatusCodes.Status400BadRequest)
+.Produces(StatusCodes.Status500InternalServerError);
 
 app.Run();
