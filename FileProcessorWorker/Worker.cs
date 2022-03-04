c04c828243b7fbd2c76d4baaa496ccd135057cd8
Felipe <felipe@meuportfol.io>
2022-03-03 22:07:48 -0300

    troca de consoleapp para worker

---
+++ FileProcessorWorker/Worker.cs
@@ -0,0 +1,97 @@
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+namespace FileProcessorWorker;
+
+public class Worker : BackgroundService
+{
+    private IModel _channel;
+    private IConnection _connection;
+    private readonly string _hostname;
+    private readonly string _queueName;
+    private readonly string _username;
+    private readonly string _password;
+    private readonly int _port;
+    private readonly ILogger<Worker> _logger;
+
+    public Worker(ILogger<Worker> logger)
+    {
+        _logger = logger;
+        _hostname = "localhost";
+        _queueName = "FileUpload.Sent";
+        _username = "user_mq";
+        _password = "RabbitMQ2019!";
+        _port = 5672;
+        InitializeRabbitMqListener();
+    }
+
+    private void InitializeRabbitMqListener()
+    {
+        var factory = new ConnectionFactory
+        {
+            HostName = _hostname,
+            UserName = _username,
+            Password = _password,
+            Port = _port
+        };
+
+        _connection = factory.CreateConnection();
+        _connection.ConnectionShutdown += RabbitMQ_ConnectionShutdown;
+        _channel = _connection.CreateModel();
+        _channel.QueueDeclare(queue: _queueName, durable: false, exclusive: false, autoDelete: false, arguments: null);
+    }
+
+    protected override Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        stoppingToken.ThrowIfCancellationRequested();
+
+        var consumer = new EventingBasicConsumer(_channel);
+        consumer.Received += (ch, ea) =>
+        {
+            var content = Encoding.UTF8.GetString(ea.Body.Span);
+
+            Console.WriteLine($"Processando mensagem: {content}");
+            System.Threading.Thread.Sleep(1000);
+            Console.WriteLine($"Mensagem: {content} processada");
+
+
+            _channel.BasicAck(ea.DeliveryTag, false);
+        };
+        consumer.Shutdown += OnConsumerShutdown;
+        consumer.Registered += OnConsumerRegistered;
+        consumer.Unregistered += OnConsumerUnregistered;
+        consumer.ConsumerCancelled += OnConsumerConsumerCancelled;
+
+        _channel.BasicConsume(_queueName, false, consumer);
+
+        return Task.CompletedTask;
+    }
+
+    private void OnConsumerConsumerCancelled(object sender, ConsumerEventArgs e)
+    {
+    }
+
+    private void OnConsumerUnregistered(object sender, ConsumerEventArgs e)
+    {
+    }
+
+    private void OnConsumerRegistered(object sender, ConsumerEventArgs e)
+    {
+    }
+
+    private void OnConsumerShutdown(object sender, ShutdownEventArgs e)
+    {
+    }
+
+    private void RabbitMQ_ConnectionShutdown(object sender, ShutdownEventArgs e)
+    {
+    }
+
+    public override void Dispose()
+    {
+        _channel.Close();
+        _connection.Close();
+        base.Dispose();
+    }
+}
